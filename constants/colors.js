818ff31d4140aaba496fac96c44b788c95368243
Santy Orsi <orsisantito@gmail.com>
2026-05-05 19:15:49 -0300

    proyecto base

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  amarillo: '#F5C400',
+  negro: '#1A1A1A',
+  blanco: '#FFFFFF',
+  grisClaro: '#F2F2F2',
+  grisMedio: '#D9D9D9',
+  grisTexto: '#555555',
+  error: '#E53935',
+  exito: '#43A047',
+};
+
+export default Colors;
